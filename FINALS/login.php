0a33ad0140b5bf3a591b3c75b5a372275e1e355a
Jeffrey <35595688+JeffreyRimando@users.noreply.github.com>
2018-05-02 11:33:25 +0800

    Add files via upload

--- FINALS/login.php
+++ FINALS/login.php
@@ -1,9 +1,8 @@
 <?php
-
-
 echo "<form method='post'>
 Username:<input type='text' name='user><br>
 Password:<input type='password' name='pass><br>
+<input type='submit' value='LOG IN'>
 <input type='submit' value='LOG IN'>
 </form>";
 ?>
